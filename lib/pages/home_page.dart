18912f6ab22fd7d904ada160d97a9fad2b4342f2
Toretay <abdymvzhit@gmail.com>
2025-02-04 21:44:07 +0500

    added button for getting location

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,12 +8,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final String latitude = "";
+  final String longitude = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Latitude: $latitude",
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Longitude: $longitude",
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                side: BorderSide(
+                  width: 1.0,
+                  color: Colors.black,
+                ),
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                elevation: 0.0,
+              ),
+              onPressed: () {},
+              child: Text(
+                "Get Location",
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
